e3de9f1f7dbd6286196f8d6dece1afef677f6db0
shamshul-haque <shamshul.haque.dev@gmail.com>
2023-10-19 09:14:14 +0600

    get all product from database

--- index.js
+++ index.js
@@ -52,6 +52,13 @@ async function run() {
       res.send(result);
     });
 
+    // Find brand from database
+    app.get("/product", async (req, res) => {
+      const cursor = productCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log(
